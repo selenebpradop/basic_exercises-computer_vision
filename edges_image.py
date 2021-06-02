94595660ade72a63122a25d41c3c247f5d9695cb
selenebpradop <63128281+selenebpradop@users.noreply.github.com>
2021-06-01 19:53:44 -0500

    Program where the edges in an image are detected.
    Program where the edges in an image are detected with Canny of Numpy library.

--- edges_image.py
+++ edges_image.py
@@ -2,9 +2,9 @@
 import cv2
 
 # Read the image
-imagen = cv2.imread('gatito.png',1)
+image = cv2.imread('image1.png',1) # Change 'image1.png' for the name of your image 
 # Show the original image
-cv2.imshow('Imagen original', imagen)
+cv2.imshow('Image 1', image)
 # Set the time to show the image
 cv2.waitKey(10000)
 # Close the image
@@ -12,7 +12,7 @@ cv2.destroyAllWindows()
 # Detect the edges with Canny
 edge_img = cv2.Canny(imagen,100,200)
 # Show the image with the edges highlighted
-cv2.imshow('Imagen con bordes resaltados', edge_img)
+cv2.imshow('Image with the edges highlighted', edge_img)
 # Set the time to show the image
 cv2.waitKey(10000)
 # Close the image
